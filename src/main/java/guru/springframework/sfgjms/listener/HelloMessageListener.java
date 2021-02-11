4c3cb7c1cd0a5908b74e29d29ad60eb47e29c02c
Silvia-F <Silvia-F@users.noreply.github.com>
2021-02-11 14:49:05 +0000

    Add sendAndReceive example

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -1,26 +1,45 @@
 package guru.springframework.sfgjms.listener;
 
 import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.messaging.Message;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+import java.util.UUID;
+
 import static guru.springframework.sfgjms.config.JmsConfig.MY_QUEUE;
+import static guru.springframework.sfgjms.config.JmsConfig.MY_SEND_RCV_QUEUE;
 
 @Component
+@RequiredArgsConstructor
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = MY_QUEUE) // Queue to which we want to listen to
     public void listen(@Payload HelloWorldMessage helloWorldMessage, // Specifies the message should be deserialized into an HelloWorldMessage
                        MessageHeaders headers, // Get the message headers (jms properties)
                        Message message) {
 
-        System.out.println("I got a message.");
-        System.out.println(helloWorldMessage);
-
         //Since jms is transactional, if an exception is thrown while the client is processing the message, this message will be redelivered
         //throw new RuntimeException();
+    }
+
+    @JmsListener(destination = MY_SEND_RCV_QUEUE)
+    public void listenForHello(Message message) throws JMSException {
+
+        HelloWorldMessage payload = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payload);
     }
 }
--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -1,27 +1,31 @@
 package guru.springframework.sfgjms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 import static guru.springframework.sfgjms.config.JmsConfig.MY_QUEUE;
+import static guru.springframework.sfgjms.config.JmsConfig.MY_SEND_RCV_QUEUE;
 
 @Component
+@RequiredArgsConstructor
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
-
-    public HelloSender(JmsTemplate jmsTemplate) {
-        this.jmsTemplate = jmsTemplate;
-    }
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000) // sendMessage() will be called each 2 seconds
     public void sendMessage() {
-
-        System.out.println("Sending message.");
 
         HelloWorldMessage message = HelloWorldMessage
                 .builder()
@@ -30,8 +34,33 @@ public class HelloSender {
                 .build();
 
         jmsTemplate.convertAndSend(MY_QUEUE, message);
+    }
 
-        System.out.println("Message sent.");
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
 
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        // Consumer uses a temporary queue to reply back to the sender. This is not asynchronous
+        Message received = jmsTemplate.sendAndReceive(MY_SEND_RCV_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                // Needed since there is no 'convert' as it is in the convertAndSend case
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+                    System.out.println("Sending Hello");
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("Failed.");
+                }
+            }
+        });
+        System.out.println(received.getBody(String.class));
     }
 }
